787301d2c25c4dfc82d062be5c8c32a59f3ec1fe
jangrahiten <jangrahiten3@gmail.com>
2026-09-01 00:23:52 +0530

    Socket.io initialised

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,28 @@
 import "dotenv/config";
+import {createServer} from "http";
+import {Server} from "socket.io";
+
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.CLIENT_URL || "http://localhost:3000",
+        credentials: true,
+    },
+});
+
+io.on("connection", (socket)=>{
+    console.log("Socket Connected:",socket.id);
+
+    socket.on("disconnected", ()=>{
+        console.log("Socket disconnected",socket.id);
+    });
+});
+
+httpServer.listen(PORT, () => {
 console.log(`Server running at http://localhost:${PORT}`)
 })
